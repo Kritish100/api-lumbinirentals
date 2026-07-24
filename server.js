5a2268df9bfc059f10302a46d6fec08ded009660
kritish <mail@kritishbhattarai.com.np>
2026-07-24 12:05:49 +0000

    updated the limit on express

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const UPLOAD_ROOT =
 app.use("/uploads", express.static(UPLOAD_ROOT));
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 // =================================
 // API Key Middleware
