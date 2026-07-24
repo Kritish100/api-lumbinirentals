f8bee5ee2596b1b0430fb5bd8d5202ba6d49b6a4
kritish <mail@kritishbhattarai.com.np>
2026-07-24 12:14:32 +0000

    used multer to handle files

--- server.js
+++ server.js
@@ -14,11 +14,10 @@ const API_KEY = process.env.API_KEY || "default_api_key";
 // 1. Serve uploaded files statically so the frontend can display them
 const UPLOAD_ROOT =
   process.env.UPLOAD_ROOT || path.join(process.cwd(), "uploads");
-app.use("/uploads", express.static(UPLOAD_ROOT));
 
+
+app.use("/uploads", express.static(UPLOAD_ROOT));
 app.use(cors());
-app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 // =================================
 // API Key Middleware
@@ -36,7 +35,12 @@ const verifyApiKey = (req, res, next) => {
 // =================================
 
 // FILE UPLOAD
-app.use("/api/properties/assets/:propertyId", assetsRouter);
+// Multer for FormData Payload
+app.use("/api/properties/assets/:propertyId", verifyApiKey, assetsRouter);
+
+// For JSON Payload
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
